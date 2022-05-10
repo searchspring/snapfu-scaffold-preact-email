8b0f10d06ca7a09649140807a89be942266cc787
kevin <5060400+korgon@users.noreply.github.com>
2022-05-10 14:40:48 -0600

    Update Email.jsx
    Adding conditional to component.

--- src/components/Recommendations/Email.jsx
+++ src/components/Recommendations/Email.jsx
@@ -15,13 +15,13 @@ export class Email extends Component {
 				},
 			},
 		};
-		return (
+		return store.results.length && (
 			<div>
 				{store.results.map((result, idx) => (
 					//****** IMPORTANT  *******//
 					// THIS OUTER "ss-emailrec" WRAPPER IS REQUIRED FOR EMAIL RECS TO WORK PROPERLY.
 					// DO NOT REMOVE OR EDIT IT
-					<div key={idx} id={`ss-emailrec${idx}`} style={{ display: 'block', width: '240px' }}>
+					<div key={idx} id={\`ss-emailrec\${idx}\`} style={{ display: 'block', width: '240px' }}>
 						{/* make your result changes here  */}
 						<Result result={result} theme={theme}></Result>
 					</div>
